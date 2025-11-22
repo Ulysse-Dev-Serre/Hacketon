4f7c846ed4f318f0a077b889fdf2cd4447f29471
sumtl <wansuwang@gmail.com>
2025-11-22 15:34:15 -0500

    Add tournaments my_stats API

--- backend/tournaments/serializers.py
+++ backend/tournaments/serializers.py
@@ -14,7 +14,9 @@ class TeamSerializer(serializers.ModelSerializer):
     """
     Serializer for the teams.
     """
+    available_spots = serializers.IntegerField(read_only=True)
+    is_full = serializers.BooleanField(read_only=True)
     class Meta:
         model = Team
         fields = ['id', 'name', 'tournament', 'max_capacity', 'current_capacity', 'members', 'created_at','available_spots', 'is_full']
-        read_only_fields = ['id', 'created_at']
+        read_only_fields = ['id', 'created_at', 'available_spots', 'is_full', 'current_capacity']
